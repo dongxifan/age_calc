b48c227938dcee14995d20c7a5ce21acd5a2b928
dongxf <dongxf@asiainfo.com>
2023-03-23 23:43:30 +0800

    feat：添加npm构建、引入vant weapp组件库

---
+++ pages/home/home.js
@@ -0,0 +1,110 @@
+
+// pages/home/home.js
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1900; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    calcDate: '1996-12-25',
+    showDatePanel: false,
+    years,
+    year: date.getFullYear(),
+    months,
+    month: date.getMonth() + 1,
+    days,
+    day: date.getDate() ,
+    value: [9999, 1, 1],
+    isDaytime: true,
+  },
+  bindChange:function() {
+    const val = e.detail.value
+    this.setData({
+      year: this.data.years[val[0]],
+      month: this.data.months[val[1]],
+      day: this.data.days[val[2]],
+      isDaytime: !val[3]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+  // 弹出日期选择框
+  showDate: function(){
+    console.log('------');
+    this.setData({showDatePanel : true})
+  },
+  // 关闭日期选择框
+  closeDate: function(){
+    this.showDatePanel = false
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
